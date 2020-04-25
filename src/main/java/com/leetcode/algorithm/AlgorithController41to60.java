64338fc770205504f2b78277843a8a5748fa4630
wanghao <wanghao1@actionsky.com>
2020-04-26 00:01:32 +0800

    complete leetcode 54. Spiral Matrix

--- src/main/java/com/leetcode/algorithm/AlgorithController41to60.java
+++ src/main/java/com/leetcode/algorithm/AlgorithController41to60.java
@@ -2,18 +2,20 @@ package com.leetcode.algorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class AlgorithController41to60 {
 
     public void rotate(int[][] matrix) {
-//        int[][] result = new int[matrix.length][matrix.length];
-//        for(int i = 0; i < matrix.length; i++) {
-//            for(int y = 0; y < matrix[i].length; y++) {
-//                result[matrix.length - 1 - i][y] = matrix[i][y];
-//            }
-//        }
-//        matrix = result;
+        // int[][] result = new int[matrix.length][matrix.length];
+        // for(int i = 0; i < matrix.length; i++) {
+        // for(int y = 0; y < matrix[i].length; y++) {
+        // result[matrix.length - 1 - i][y] = matrix[i][y];
+        // }
+        // }
+        // matrix = result;
     }
 
     public int lengthOfLastWord(String s) {
@@ -36,7 +38,8 @@ public class AlgorithController41to60 {
         return result;
     }
 
-    public void combinationSum2Impl(List<List<Integer>> resultList, List<Integer> currentList, int[] candidates, int startIndex, int target) {
+    public void combinationSum2Impl(List<List<Integer>> resultList, List<Integer> currentList, int[] candidates,
+            int startIndex, int target) {
         if (target > 0) {
             for (int i = startIndex; i < candidates.length; i++) {
                 if (i > startIndex && candidates[i] == candidates[i - 1]) {
@@ -53,7 +56,7 @@ public class AlgorithController41to60 {
 
     public int firstMissingPositive(int[] nums) {
         int[] tempNums = new int[nums.length + 1];
-        for (int i = 0; i < nums.length;i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
                 continue;
             }
@@ -66,38 +69,32 @@ public class AlgorithController41to60 {
         }
         return tempNums.length;
     }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (matrix.length == 0) {
+            return result;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        int time = 0;
+        while (time < row && time < column) {
+            for (int i = time; i < column; i++) {
+                result.add(matrix[time][i]);
+            }
+            for (int y = time + 1; y < row; y++) {
+                result.add(matrix[y][column - 1]);
+            }
+            for (int x = column - 2; x >= time && (row != time + 1); x--) {
+                result.add(matrix[row - 1][x]);
+            }
+            for (int z = row - 2; z > time && (column != time + 1); z--) {
+                result.add(matrix[z][time]);
+            }
+            time++;
+            row--;
+            column--;
+        }
+        return result;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/test/java/com/leetcode/algorithm/AlgorithController41to60Test.java
+++ src/test/java/com/leetcode/algorithm/AlgorithController41to60Test.java
@@ -1,8 +1,17 @@
 package com.leetcode.algorithm;
 
 import com.leetcode.tool.Print;
+
+import org.json.JSONArray;
+import org.junit.Assert;
 import org.junit.Test;
 
+import net.minidev.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -13,10 +22,10 @@ public class AlgorithController41to60Test {
 
     @Test
     public void rotate() {
-        int[] test1 = new int[]{1,2,3};
-        int[] test2 = new int[]{4,5,6};
-        int[] test3 = new int[]{7,8,9};
-        int[][] test = new int[][]{test1,test2,test3};
+        int[] test1 = new int[] { 1, 2, 3 };
+        int[] test2 = new int[] { 4, 5, 6 };
+        int[] test3 = new int[] { 7, 8, 9 };
+        int[][] test = new int[][] { test1, test2, test3 };
         Print.print2DIntArray(test);
         this.algorithController41to60.rotate(test);
         Print.print2DIntArray(test);
@@ -30,15 +39,42 @@ public class AlgorithController41to60Test {
 
     @Test
     public void combinationSum() {
-        int[] test = new int[]{10,1,2,7,6,1,5};
+        int[] test = new int[] { 10, 1, 2, 7, 6, 1, 5 };
         List<List<Integer>> result = this.algorithController41to60.combinationSum2(test, 8);
         System.out.println(result);
     }
 
     @Test
     public void firstMissingPositive() {
-        int[] test = new int[]{1,2};
+        int[] test = new int[] { 1, 2 };
         int result = this.algorithController41to60.firstMissingPositive(test);
         System.out.println(result);
     }
+
+    @Test
+    public void spiralOrder() {
+        int[][] test = new int[3][3];
+        test[0] = new int[] { 1, 2, 3 };
+        test[1] = new int[] { 4, 5, 6 };
+        test[2] = new int[] { 7, 8, 9 };
+        List res1 = new ArrayList<Integer>();
+        Collections.addAll(res1, 1, 2, 3, 6, 9, 8, 7, 4, 5);
+        Assert.assertEquals(algorithController41to60.spiralOrder(test), res1);
+
+        int[][] test2 = new int[3][4];
+        test2[0] = new int[] { 1, 2, 3, 4 };
+        test2[1] = new int[] { 5, 6, 7, 8 };
+        test2[2] = new int[] { 9, 10, 11, 12 };
+        List res2 = new ArrayList<Integer>();
+        Collections.addAll(res2, 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7);
+        Assert.assertEquals(algorithController41to60.spiralOrder(test2), res2);
+
+        int[][] test3 = new int[1][2];
+        test3[0] = new int[] { 2, 3 };
+        List res3 = new ArrayList<Integer>();
+        Collections.addAll(res3, 2, 3);
+        Assert.assertEquals(algorithController41to60.spiralOrder(test3), res3);
+
+    }
+
 }
