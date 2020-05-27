5c47dac8c72fc1b4a7bd561a48985f71a2f389fc
wanghao <wanghao1@actionsky.com>
2020-05-27 13:40:13 +0800

    complete question 74. Search a 2D Matrix

--- src/main/java/com/leetcode/algorithm/AlgorithController61to80.java
+++ src/main/java/com/leetcode/algorithm/AlgorithController61to80.java
@@ -358,4 +358,36 @@ public class AlgorithController61to80 {
     }
   }
 
+  public boolean searchMatrix(int[][] matrix, int target) {
+    for (int i = 0; i < matrix.length; i++) {
+      if (matrix[i].length == 0) {
+        continue;
+      }
+      if (matrix[i][0] <= target && matrix[i][matrix[i].length - 1] >= target) {
+        return findTargetInArray(matrix[i], target);
+      }
+    }
+    return false;
+  }
+
+  public boolean findTargetInArray(int[] arr, int target) {
+    if (arr.length < 1) {
+      return false;
+    }
+    int left = 0;
+    int right = arr.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) {
+        return true;
+      }
+      if (arr[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return false;
+  }
+
 }
