f1bc329826b94b3f1dcb50d05fd89d84c2e3f98e
Rain <13131459087@163.com>
2020-06-10 18:52:39 +0800

    complete question 82. Remove Duplicates from Sorted List II (#28)
    Co-authored-by: wanghao <wanghao1@actionsky.com>

--- src/test/java/com/leetcode/algorithm/AlgorithmController81to100Test.java
+++ src/test/java/com/leetcode/algorithm/AlgorithmController81to100Test.java
@@ -1,5 +1,7 @@
 package com.leetcode.algorithm;
 
+import com.leetcode.entity.ListNode;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,7 +11,19 @@ public class AlgorithmController81to100Test {
 
   @Test
   public void search() {
-    Assert.assertEquals(this.algorithmController81to100.search(new int[]{2,5,6,0,0,1,2}, 3), false);
-    Assert.assertEquals(this.algorithmController81to100.search(new int[]{3,1,1}, 3), true);
+    Assert.assertEquals(this.algorithmController81to100.search(new int[] { 2, 5, 6, 0, 0, 1, 2 }, 3), false);
+    Assert.assertEquals(this.algorithmController81to100.search(new int[] { 3, 1, 1 }, 3), true);
+  }
+
+  @Test
+  public void deleteDuplicates() {
+    ListNode test1 = new ListNode(1);
+    test1.next = new ListNode(2);
+    test1.next.next = new ListNode(3);
+    test1.next.next.next = new ListNode(3);
+    test1.next.next.next.next = new ListNode(4);
+    test1.next.next.next.next.next = new ListNode(4);
+    test1.next.next.next.next.next.next = new ListNode(5);
+    System.out.println(this.algorithmController81to100.deleteDuplicates(test1).toString());
   }
 }
