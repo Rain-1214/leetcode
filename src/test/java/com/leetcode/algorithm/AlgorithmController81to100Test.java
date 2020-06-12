629d858eade6a653320ab25646ee9e53720317c6
wanghao <wanghao1@actionsky.com>
2020-06-12 13:06:27 +0800

    complete question 84.Largest Rectangle in Histogram

--- src/test/java/com/leetcode/algorithm/AlgorithmController81to100Test.java
+++ src/test/java/com/leetcode/algorithm/AlgorithmController81to100Test.java
@@ -26,4 +26,15 @@ public class AlgorithmController81to100Test {
     test1.next.next.next.next.next.next = new ListNode(5);
     System.out.println(this.algorithmController81to100.deleteDuplicates(test1).toString());
   }
+
+  @Test
+  public void largestRectangleArea() {
+    int[] test1 = new int[]{2,1,5,6,2,3};
+    Assert.assertEquals(this.algorithmController81to100.largestRectangleArea(test1), 10);
+    int[] test2 = new int[]{6};
+    Assert.assertEquals(this.algorithmController81to100.largestRectangleArea(test2), 6);
+    int[] test3 = new int[]{5,5,1,7,1,1,5,2,7,6};
+    Assert.assertEquals(this.algorithmController81to100.largestRectangleArea(test3), 12);
+  }
+
 }
