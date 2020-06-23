d55b9586b86757c9f966b1e7a57fb2b78290cceb
wanghao <wanghao1@actionsky.com>
2020-06-23 13:22:46 +0800

    complete question 88. merge sorted array

--- src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
@@ -181,5 +181,15 @@ public class AlgorithmController81to100 {
     }
   }
 
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int i = m - 1, j = n - 1, x = m + n -1;
+    while(j >= 0) {
+      if (i >= 0) {
+        nums1[x--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+      } else {
+        nums1[x--] = nums2[j--];
+      }
+    }
+  }
 
 }
