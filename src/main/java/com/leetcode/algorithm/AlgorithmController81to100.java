9c601df313036fc9493c33bfdf812eabc2746bab
wanghao <wanghao1@actionsky.com>
2020-06-28 12:48:51 +0800

    complete question 89 gray code

--- src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
@@ -192,4 +192,24 @@ public class AlgorithmController81to100 {
     }
   }
 
+  public List<Integer> grayCodeTooMath(int n) {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < 1 << n; i++) {
+      res.add(i ^ i >> 1);
+    }
+    return res;
+  }
+
+  public List<Integer> grayCode(int n) {
+    List<Integer> res = new ArrayList<>();
+    res.add(0);
+    for (int i = 1; i <= n; i++) {
+      int num = 1 << (i - 1);
+      for (int j = res.size() - 1; j >= 0; j--) {
+        res.add(res.get(j) + num);
+      }
+    }
+    return res;
+  }
+
 }
