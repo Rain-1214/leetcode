159f9f15a513f4efbd3d9458f559940a1a74d1c0
wanghao <wanghao1@actionsky.com>
2020-07-28 11:19:47 +0800

    complete question 100. Same Tree

--- src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController81to100.java
@@ -495,4 +495,14 @@ public class AlgorithmController81to100 {
     recoverTreeImpl(root.right);
   }
 
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null || q == null) {
+      return p == q;
+    }
+    if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
 }
