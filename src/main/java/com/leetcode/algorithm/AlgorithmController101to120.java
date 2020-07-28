988f71cc97868442840574156add168dede20797
wanghao <wanghao1@actionsky.com>
2020-07-28 13:33:22 +0800

    complete quetion 102. Binary Tree Level Order Traversal

--- src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
@@ -1,5 +1,8 @@
 package com.leetcode.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.leetcode.entity.TreeNode;
 
 public class AlgorithmController101to120 {
@@ -18,6 +21,30 @@ public class AlgorithmController101to120 {
       return false;
     }
     return isSymmetricImpl(left.left, right.right) && isSymmetricImpl(left.right, right.left);
+  }
+
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    levelOrderImpl(root, 0, res);
+    return res;
+  }
+
+  public void levelOrderImpl(TreeNode node, int level, List<List<Integer>> res) {
+    if (node == null) {
+      return;
+    }
+    if (res.size() <= level) {
+      List<Integer> temp = new ArrayList<>();
+      temp.add(node.val);
+      res.add(temp);
+    } else {
+      res.get(level).add(node.val);
+    }
+    levelOrderImpl(node.left, level + 1, res);
+    levelOrderImpl(node.right, level + 1, res);
   }
 
 }
