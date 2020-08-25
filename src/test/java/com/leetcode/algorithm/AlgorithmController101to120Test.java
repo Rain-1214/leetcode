501f7375c268c547fb53d7c90433fb5dc61d7b70
wanghao <wanghao1@actionsky.com>
2020-08-25 18:13:58 +0800

    complete 106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ src/test/java/com/leetcode/algorithm/AlgorithmController101to120Test.java
@@ -0,0 +1,25 @@
+package com.leetcode.algorithm;
+
+import com.leetcode.entity.TreeNode;
+
+import org.junit.Test;
+
+public class AlgorithmController101to120Test {
+
+  private AlgorithmController101to120 algorithmController81to100 = new AlgorithmController101to120();
+
+  @Test
+  public void buildTree() {
+    int[] preorder = new int[] { 1, 2, 4, 5, 3, 6, 7 };
+    int[] inorder = new int[] { 4, 2, 5, 1, 6, 3, 7 };
+    TreeNode a = this.algorithmController81to100.buildTree(preorder, inorder);
+  }
+
+  @Test
+  public void buildTree106() {
+    int[] inorder = new int[] { 4, 2, 5, 1, 6, 3, 7 };
+    int[] postorder = new int[] { 4, 5, 2, 6, 7, 3, 1 };
+    TreeNode a = this.algorithmController81to100.buildTree106(inorder, postorder);
+  }
+
+}
