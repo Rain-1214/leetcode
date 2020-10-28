d42bb3f04f2764aee87475ebcba27e43a1f60b8d
Rain <13131459087@163.com>
2020-10-28 12:46:23 +0800

    complete question 114. Flatten Binary Tree to Linked List (#59)
    Co-authored-by: wanghao <wanghao1@actionsky.com>

--- src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
@@ -324,4 +324,24 @@ public class AlgorithmController101to120 {
     list.removeLast();
   }
 
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    if (root.left != null) {
+      flatten(root.left);
+    }
+    if (root.right != null) {
+      flatten(root.right);
+    }
+    TreeNode temp = root.right;
+    root.right = root.left;
+    root.left = null;
+    TreeNode current = root;
+    while (current.right != null) {
+      current = current.right;
+    }
+    current.right = temp;
+  }
+
 }
