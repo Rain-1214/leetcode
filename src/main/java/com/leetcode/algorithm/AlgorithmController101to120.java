ab5724cbfa2767d3bb54f8d94364cd9630c4d4f6
wanghao <wanghao1@actionsky.com>
2020-11-03 12:34:47 +0800

    complete question 116. Populating Next Right Pointers in Each Node

--- src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
@@ -436,6 +436,14 @@ public class AlgorithmController101to120 {
     if (root == null) {
       return root;
     }
+    if (root.left != null) {
+      root.left.next = root.right;
+      if (root.next != null) {
+        root.right.next = root.next.left;
+      }
+    }
+    connect(root.left);
+    connect(root.right);
     return root;
   }
 
