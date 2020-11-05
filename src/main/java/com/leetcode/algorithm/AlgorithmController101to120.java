69ab47a47ad071e26b2e8b13c9b75cbd2575e7a1
wanghao <wanghao1@actionsky.com>
2020-11-05 12:50:32 +0800

    complete question 121. Best Time to Buy and Sell Stock

--- src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController101to120.java
@@ -574,4 +574,20 @@ public class AlgorithmController101to120 {
     return min;
   }
 
+  public int maxProfit(int[] prices) {
+    if (prices.length <= 1) {
+      return 0;
+    }
+    int minBuy = prices[0];
+    int res = 0;
+    for (int i = 1; i < prices.length; i++) {
+      if (prices[i] > minBuy) {
+        res = Math.max(prices[i] - minBuy, res);
+      } else {
+        minBuy = prices[i];
+      }
+    }
+    return res;
+  }
+
 }
