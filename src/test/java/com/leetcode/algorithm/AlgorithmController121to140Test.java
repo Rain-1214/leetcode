78486cb7347523a815564a0741039b8ee0b55f19
wanghao <wanghao1@actionsky.com>
2020-11-27 12:58:48 +0800

    complete question 127. Word Ladder

--- src/test/java/com/leetcode/algorithm/AlgorithmController121to140Test.java
+++ src/test/java/com/leetcode/algorithm/AlgorithmController121to140Test.java
@@ -33,13 +33,14 @@ public class AlgorithmController121to140Test {
     case1.add("log");
     case1.add("cog");
     List<List<String>> res = this.algorithmController121to140.findLadders("hit", "cog", case1);
-    System.out.println(res);
     List<String> case2 = new ArrayList<>();
     case2.add("a");
     case2.add("b");
     case2.add("c");
     List<List<String>> res2 = this.algorithmController121to140.findLadders("a", "c", case2);
-    System.out.println(res2);
+
+    int res3 = this.algorithmController121to140.ladderLength("a", "c", case2);
+    System.out.println(res3);
   }
 
 }
