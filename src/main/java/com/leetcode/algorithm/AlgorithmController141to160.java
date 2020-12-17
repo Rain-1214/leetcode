567e8b550d44d7ebae96e66fd3fb97982db064ee
wanghao <wanghao1@actionsky.com>
2020-12-17 12:46:17 +0800

    complete question 142. Linked List Cycle II

--- src/main/java/com/leetcode/algorithm/AlgorithmController141to160.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController141to160.java
@@ -37,4 +37,45 @@ public class AlgorithmController141to160 {
     }
     return false;
   }
+
+  public ListNode detectCycleTooSlow(ListNode head) {
+    return detectCycle(head, new HashSet<>());
+  }
+
+  public ListNode detectCycle(ListNode head, Set<ListNode> set) {
+    if (head.next == null) {
+      return null;
+    }
+    if (set.contains(head)) {
+      return head;
+    }
+    set.add(head);
+    return detectCycle(head.next, set);
+  }
+
+  /**
+   * 2 * slow(distance) = fast(distance)
+   * https://web.archive.org/web/20160401024212/http://learningarsenal.info:80/index.php/2015/08/24/detecting-start-of-a-loop-in-singly-linked-list/
+   */
+  public ListNode detectCycle(ListNode head) {
+    if (head == null || head.next == null || head.next.next == null) {
+      return null;
+    }
+    ListNode slow = head.next;
+    ListNode fast = head.next.next;
+    while (slow != fast) {
+      if (fast.next == null || fast.next.next == null) {
+        return null;
+      }
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    slow = head;
+    while (slow != fast) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    return slow;
+  }
+
 }
