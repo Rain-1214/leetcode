c9d87746b715d2e139239a997d29b6a740c6ed0f
Rain <wanghao1@actionsky.com>
2020-12-17 19:11:46 +0800

    complete question 145. Binary Tree Postorder Traversal (#91)

--- src/main/java/com/leetcode/algorithm/AlgorithmController141to160.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController141to160.java
@@ -167,4 +167,19 @@ public class AlgorithmController141to160 {
     preorderTraversal(root.right, res);
   }
 
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    postorderTraversal(root, res);
+    return res;
+  }
+
+  public void postorderTraversal(TreeNode root, List<Integer> res) {
+    if (root == null) {
+      return;
+    }
+    postorderTraversal(root.left, res);
+    postorderTraversal(root.right, res);
+    res.add(root.val);
+  }
+
 }
