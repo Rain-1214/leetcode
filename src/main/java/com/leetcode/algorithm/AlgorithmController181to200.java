ad03cf6861fcc262b9018bc83da3955f02d97681
wanghao <wanghao1@actionsky.com>
2021-01-12 23:47:43 +0800

    complete question 188

--- src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
@@ -1,9 +1,12 @@
 package com.leetcode.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import com.leetcode.tool.Print;
 
 public class AlgorithmController181to200 {
   public void reverseWords(char[] s) {
@@ -44,4 +47,30 @@ public class AlgorithmController181to200 {
     }
     return res;
   }
+
+  public int maxProfit(int k, int[] prices) {
+    if (prices.length < 2) {
+      return 0;
+    }
+    int n = prices.length;
+    k = Math.min(n / 2, k);
+
+    int[] buy = new int[k + 1];
+    int[] sell = new int[k + 1];
+    buy[0] = -prices[0];
+    for (int i = 1; i <= k; i++) {
+      buy[i] = sell[i] = Integer.MIN_VALUE / 2;
+    }
+
+    for (int i = 1; i < prices.length; i++) {
+      int p = prices[i];
+      buy[0] = Math.max(buy[0], -p);
+      for (int j = 1; j <= k; j++) {
+        buy[j] = Math.max(buy[j], sell[j] - p);
+        sell[j] = Math.max(sell[j], buy[j - 1] + p);
+      }
+    }
+    return sell[k];
+  }
+
 }
