8102c9d24c82fb08b5a6abf8486f5c825d5b5703
Rain <wanghao1@actionsky.com>
2021-01-12 12:21:22 +0800

    complete question 179. Largest Number (#121)

---
+++ src/test/java/com/leetcode/algorithm/AlgorithmController161to180Test.java
@@ -0,0 +1,16 @@
+package com.leetcode.algorithm;
+
+import org.junit.Test;
+
+public class AlgorithmController161to180Test {
+
+  private AlgorithmController161to180 algorithmController161to180 = new AlgorithmController161to180();
+
+  @Test
+  public void largestNumber() {
+    int[] test = new int[] { 3, 30, 34, 5, 9 };
+    // this.algorithmController161to180.largestNumber(test);
+    System.out.println(this.algorithmController161to180.largestNumber(test));
+  }
+
+}
