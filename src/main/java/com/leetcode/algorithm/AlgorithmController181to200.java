6725fcaaa79ce635bdaa0461336395233dfdb7d4
Rain <wanghao1@actionsky.com>
2021-01-13 22:13:09 +0800

    complete question 198. House Robber (#129)

--- src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
@@ -172,4 +172,15 @@ public class AlgorithmController181to200 {
     return res;
   }
 
+  public int rob(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+    int[] dp = new int[nums.length + 2];
+    for (int i = 2; i < nums.length; i++) {
+      dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1]);
+    }
+    return dp[dp.length - 1];
+  }
+
 }
