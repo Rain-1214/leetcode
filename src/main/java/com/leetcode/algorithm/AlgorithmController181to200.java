298ccdc6761d20489c682c4fa6f47c9cb60a046d
Rain <wanghao1@actionsky.com>
2021-01-13 21:57:14 +0800

    complete question 191. Number of 1 Bits (#127)

--- src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController181to200.java
@@ -160,4 +160,16 @@ public class AlgorithmController181to200 {
     return res;
   }
 
+  public int hammingWeight(int n) {
+    int res = 0;
+    int i = 32;
+    while (i-- > 0) {
+      if ((1 & n) == 1) {
+        res++;
+      }
+      n >>= 1;
+    }
+    return res;
+  }
+
 }
