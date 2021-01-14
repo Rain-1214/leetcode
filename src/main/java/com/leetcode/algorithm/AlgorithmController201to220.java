8360323b0d2b2eb432ac8e33adf2068a403035c7
Rain <wanghao1@actionsky.com>
2021-01-14 12:32:03 +0800

    complete question 202. Happy Number (#133)

--- src/main/java/com/leetcode/algorithm/AlgorithmController201to220.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController201to220.java
@@ -1,5 +1,8 @@
 package com.leetcode.algorithm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AlgorithmController201to220 {
 
   public int rangeBitwiseAnd(int m, int n) {
@@ -17,6 +20,28 @@ public class AlgorithmController201to220 {
       bit++;
     }
     return n << bit;
+  }
+
+  public boolean isHappy(int n) {
+    Set<Integer> cache = new HashSet<>();
+    while (true) {
+      n = sumSquaresOfDigits(n);
+      if (n == 1) {
+        return true;
+      } else if (cache.contains(n)) {
+        return false;
+      }
+      cache.add(n);
+    }
+  }
+
+  public int sumSquaresOfDigits(int n) {
+    int res = 0;
+    while (n > 0) {
+      res += Math.pow(n % 10, 2);
+      n /= 10;
+    }
+    return res;
   }
 
 }
