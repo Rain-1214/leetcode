7186d137eb9ca5a921fc4595c9b5246371142b0b
wanghao <wanghao1@actionsky.com>
2021-01-19 12:47:09 +0800

    complete question 208. Implement Trie (Prefix Tree)

--- src/main/java/com/leetcode/algorithm/AlgorithmController201to220.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController201to220.java
@@ -230,4 +230,85 @@ public class AlgorithmController201to220 {
     return visitor == numCourses;
   }
 
+  class Trie {
+
+    class TrieNode {
+      private TrieNode[] nodes;
+      private boolean isEnd;
+
+      public TrieNode() {
+        nodes = new TrieNode[26];
+        isEnd = false;
+      }
+
+      public boolean containsKey(char c) {
+        return nodes[c - 'a'] != null;
+      }
+
+      public TrieNode getNode(char c) {
+        return nodes[c - 'a'];
+      }
+
+      public void putNode(char c, TrieNode node) {
+        nodes[c - 'a'] = node;
+      }
+
+      public void setIsEnd() {
+        isEnd = true;
+      }
+
+      public boolean getIsEnd() {
+        return isEnd;
+      }
+
+    }
+
+    public TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+      root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+      TrieNode node = root;
+      for (int i = 0; i < word.length(); i++) {
+        char c = word.charAt(i);
+        if (!node.containsKey(c)) {
+          node.putNode(c, new TrieNode());
+        }
+        node = node.getNode(c);
+      }
+      node.setIsEnd();
+    }
+
+    public TrieNode getPrefix(String word) {
+      TrieNode node = root;
+      for (int i = 0; i < word.length(); i++) {
+        char c = word.charAt(i);
+        if (node.containsKey(c)) {
+          node = node.getNode(c);
+        } else {
+          return null;
+        }
+      }
+      return node;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+      TrieNode node = getPrefix(word);
+      return node != null && node.getIsEnd();
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+      TrieNode node = getPrefix(prefix);
+      return node != null;
+    }
+  }
+
 }
