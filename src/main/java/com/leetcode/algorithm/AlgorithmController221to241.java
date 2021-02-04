6467ae8875b329282939cf23830da33e97a28b61
Rain <wanghao1@actionsky.com>
2021-02-04 12:48:42 +0800

    complete question 221. Maximal Square (#152)

---
+++ src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
@@ -0,0 +1,25 @@
+package com.leetcode.algorithm;
+
+public class AlgorithmController221to241 {
+  public int maximalSquare(char[][] matrix) {
+    int y = matrix.length;
+    int x = matrix[0].length;
+    int[][] dp = new int[y][x];
+    int res = 0;
+
+    for (int i = 0; i < y; i++) {
+      for (int j = 0; j < x; j++) {
+        if (matrix[i][j] == '1') {
+          if (i == 0 || j == 0) {
+            dp[i][j] = 1;
+          } else {
+            dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i - 1][j - 1]), dp[i][j - 1]) + 1;
+          }
+          res = Math.max(dp[i][j], res);
+        }
+
+      }
+    }
+    return res * res;
+  }
+}
