edf616697e0c09ed4f6092e19088c936be9bcae7
wanghao <wanghao1@actionsky.com>
2021-02-05 11:53:47 +0800

    complete question 223. Rectangle Area

--- src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
@@ -40,4 +40,16 @@ public class AlgorithmController221to241 {
     this.countNodes(root.left);
     this.countNodes(root.right);
   }
+
+  public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    if (E >= C || F >= D || A >= G || B >= H) {
+      return (C - A) * (D - B) + (G - E) * (H - F);
+    }
+    int left = Math.max(A, E);
+    int right = Math.min(C, G);
+    int top = Math.min(D, H);
+    int bottom = Math.max(B, F);
+    return (C - A) * (D - B) - (right - left) * (top - bottom) + (G - E) * (H - F);
+  }
+
 }
