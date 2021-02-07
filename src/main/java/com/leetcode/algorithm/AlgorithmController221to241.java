552b05607f7db83da9cb0239d94e369fa59e1094
Rain <wanghao1@actionsky.com>
2021-02-07 12:52:24 +0800

    complete question 226. Invert Binary Tree (#157)

--- src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
@@ -131,4 +131,16 @@ public class AlgorithmController221to241 {
     }
   }
 
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    invertTree(root.left);
+    invertTree(root.right);
+    TreeNode temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+    return root;
+  }
+
 }
