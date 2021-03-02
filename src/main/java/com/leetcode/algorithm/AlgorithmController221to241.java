c5c8b25f9f18822fcc22d9f4bc060825a4fc8c25
wanghao <wanghao1@actionsky.com>
2021-03-02 12:44:33 +0800

    complete question 231 Power of Two

--- src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController221to241.java
@@ -269,4 +269,20 @@ public class AlgorithmController221to241 {
     return -1;
   }
 
+  public boolean isPowerOfTwo(int n) {
+    if (n == 0) {
+      return false;
+    }
+    long x = (long) n;
+    return (x & (x - 1)) == 0;
+  }
+
+  public boolean isPowerOfTwoII(int n) {
+    if (n == 0) {
+      return false;
+    }
+    long x = (long) n;
+    return (x & -x) == x;
+  }
+
 }
