44368da1570589e89903bb8891d65e0df9debf26
wanghao <wanghao1@actionsky.com>
2021-03-16 12:29:59 +0800

    complete question 242. Valid Anagram

--- src/main/java/com/leetcode/algorithm/AlgorithmController241to260.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController241to260.java
@@ -47,4 +47,38 @@ public class AlgorithmController241to260 {
     }
     return res;
   }
+
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+    int[] table = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+      table[s.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < s.length(); i++) {
+      table[t.charAt(i) - 'a']--;
+      if (table[t.charAt(i) - 'a'] < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public boolean isAnagramII(String s, String t) {
+    if (s.length() != t.length()) {
+      return false;
+    }
+    int[] table = new int[26];
+    for (int c : s.toCharArray()) {
+      table[c - 'a']++;
+    }
+    for (int c : t.toCharArray()) {
+      table[c - 'a']--;
+      if (table[c - 'a'] < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
