8d9ad191495a2c2f993f2332fb7f344fdf84c349
wanghao <wanghao1@actionsky.com>
2021-04-02 12:34:12 +0800

    complete question 258. Add Digits

--- src/main/java/com/leetcode/algorithm/AlgorithmController241to260.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController241to260.java
@@ -607,4 +607,16 @@ public class AlgorithmController241to260 {
     }
   }
 
+  public int addDigits(int num) {
+    if (num < 10) {
+      return num;
+    }
+    int res = 0;
+    while (num > 0) {
+      res += num % 10;
+      num /= 10;
+    }
+    return res < 10 ? res : addDigits(res);
+  }
+
 }
