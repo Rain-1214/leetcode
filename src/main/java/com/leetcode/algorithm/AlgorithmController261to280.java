321b82bb2516da43f9091f958d825754db2f4b4e
Rain <wanghao1@actionsky.com>
2021-04-27 12:40:49 +0800

    complete question 278. First Bad Version (#207)

--- src/main/java/com/leetcode/algorithm/AlgorithmController261to280.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController261to280.java
@@ -614,4 +614,22 @@ public class AlgorithmController261to280 {
     return res;
   }
 
+  public boolean isBadVersion(int version) {
+    return false;
+  }
+
+  public int firstBadVersion(int n) {
+    int start = 1;
+    int end = n;
+    while (start < end) {
+      int mid = start + (end - start) / 2;
+      if (isBadVersion(mid)) {
+        end = mid;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  }
+
 }
