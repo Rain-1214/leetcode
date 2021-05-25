a75e780bad58b8cc6aa48d203f39042b48ad992c
wanghao <wanghao1@actionsky.com>
2021-05-25 12:37:43 +0800

    complete question 309. Best Time to Buy and Sell Stock with Cooldown

--- src/main/java/com/leetcode/algorithm/AlgorithmController301to320.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController301to320.java
@@ -516,4 +516,24 @@ public class AlgorithmController301to320 {
     }
   }
 
+  public int maxProfit(int[] prices) {
+    if (prices.length == 1) {
+      return 0;
+    }
+    int[] sell = new int[prices.length];
+    int[] buy = new int[prices.length];
+    buy[0] = -prices[0];
+    buy[1] = Math.max(buy[0], -prices[1]);
+    sell[1] = Math.max(sell[0], buy[0] + prices[1]);
+    for (int i = 2; i < prices.length; i++) {
+      sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+      buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
+    }
+    int max = Integer.MIN_VALUE;
+    for (int n : sell) {
+      max = Math.max(max, n);
+    }
+    return max;
+  }
+
 }
