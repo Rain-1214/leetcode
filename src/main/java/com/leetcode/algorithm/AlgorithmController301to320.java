d8687b4a9c124f4c8fb2d4c4e5df259b4d485927
wanghao <wanghao1@actionsky.com>
2021-05-27 12:49:56 +0800

    complete question 312. Burst Balloons

--- src/main/java/com/leetcode/algorithm/AlgorithmController301to320.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController301to320.java
@@ -605,4 +605,25 @@ public class AlgorithmController301to320 {
     return res;
   }
 
+  public int maxCoins(int[] nums) {
+    int n = nums.length;
+    int[][] dp = new int[n + 2][n + 2];
+    int[] vals = new int[n + 2];
+    vals[0] = vals[n + 1] = 1;
+    for (int i = 1; i < n + 1; i++) {
+      vals[i] = nums[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = i + 2; j < n + 2; j++) {
+        int temp = vals[i] * vals[j];
+        int max = 0;
+        for (int k = i + 1; k < j; k++) {
+          max = Math.max(max, temp * vals[k] + dp[i][k] + dp[k][j]);
+        }
+        dp[i][j] = max;
+      }
+    }
+    return dp[0][n + 1];
+  }
+
 }
