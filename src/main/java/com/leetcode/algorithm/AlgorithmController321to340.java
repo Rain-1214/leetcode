ee83281659c3c513d91700883cad9e339be2f0e3
wanghao <wanghao1@actionsky.com>
2021-06-10 12:13:03 +0800

    complete question 326. Power of Three

--- src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
@@ -283,4 +283,18 @@ public class AlgorithmController321to340 {
     return max;
   }
 
+  public boolean isPowerOfThree(int n) {
+    if (n < 1) {
+      return false;
+    }
+    while (n % 3 == 0) {
+      n /= 3;
+    }
+    return n == 1;
+  }
+
+  public boolean isPowerOfThreeII(int n) {
+    return n > 0 && 1162261467 % n == 0;
+  }
+
 }
