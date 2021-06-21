2f80f238796ae65dbde3e40c92c2f511a1b02364
wanghao <wanghao1@actionsky.com>
2021-06-21 12:34:26 +0800

    complete question 334. Increasing Triplet Subsequence

--- src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
@@ -631,4 +631,21 @@ public class AlgorithmController321to340 {
     return res;
   }
 
+  public boolean increasingTriplet(int[] nums) {
+    if (nums.length < 3) {
+      return false;
+    }
+    int small = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] <= small) {
+        small = nums[i];
+      } else if (nums[i] <= mid) {
+        mid = nums[i];
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
