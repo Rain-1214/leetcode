30fd1d22b0776fde643881a706670c799530d954
Rain <wanghao1@actionsky.com>
2021-06-25 12:26:27 +0800

    complete question 341. Flatten Nested List Iterator (#270)

--- src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController321to340.java
@@ -2,10 +2,8 @@ package com.leetcode.algorithm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
