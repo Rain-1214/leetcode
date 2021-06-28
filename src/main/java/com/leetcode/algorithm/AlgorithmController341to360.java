811a2bef21ee4077abe605576bdfffb97803afb8
Rain <wanghao1@actionsky.com>
2021-06-28 12:57:14 +0800

    complete question 345. Reverse Vowels of a String (#274)

--- src/main/java/com/leetcode/algorithm/AlgorithmController341to360.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController341to360.java
@@ -1,8 +1,10 @@
 package com.leetcode.algorithm;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.leetcode.entity.NestedInteger;
 
@@ -75,6 +77,64 @@ public class AlgorithmController341to360 {
       left++;
       right--;
     }
+  }
+
+  public String reverseVowels(String s) {
+    char[] sa = s.toCharArray();
+    int left = 0, right = sa.length - 1;
+    Set<Character> dic = new HashSet<>();
+    dic.add('a');
+    dic.add('e');
+    dic.add('i');
+    dic.add('o');
+    dic.add('u');
+    dic.add('A');
+    dic.add('E');
+    dic.add('I');
+    dic.add('O');
+    dic.add('U');
+    while (left < right) {
+      if (!dic.contains(sa[left])) {
+        left++;
+        continue;
+      } else if (!dic.contains(sa[right])) {
+        right--;
+        continue;
+      } else {
+        char temp = sa[left];
+        sa[left] = sa[right];
+        sa[right] = temp;
+        left++;
+        right--;
+      }
+    }
+    return new String(sa);
+  }
+
+  public String reverseVowelsII(String s) {
+    char[] sa = s.toCharArray();
+    int left = 0, right = sa.length - 1;
+    while (left < right) {
+      if (!isVowels(sa[left])) {
+        left++;
+        continue;
+      } else if (!isVowels(sa[right])) {
+        right--;
+        continue;
+      } else {
+        char temp = sa[left];
+        sa[left] = sa[right];
+        sa[right] = temp;
+        left++;
+        right--;
+      }
+    }
+    return new String(sa);
+  }
+
+  public boolean isVowels(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+        || c == 'U';
   }
 
 }
