5a0f0cbf4d2b6313afa958d923e4cf6049b6a2bd
wanghao <wanghao1@actionsky.com>
2021-06-30 12:12:14 +0800

    complete question 350. Intersection of Two Arrays II

--- src/main/java/com/leetcode/algorithm/AlgorithmController341to360.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController341to360.java
@@ -301,4 +301,24 @@ public class AlgorithmController341to360 {
     }
   }
 
+  public int[] intersection(int[] nums1, int[] nums2) {
+    Set<Integer> set1 = new HashSet<>();
+    Set<Integer> newSet = new HashSet<>();
+    for (int num: nums1) {
+      set1.add(num);
+    }
+    for (int num: nums2) {
+      if (set1.contains(num)) {
+        newSet.add(num);
+      }
+    }
+    int[] res = new int[newSet.size()];
+    Iterator<Integer> i = newSet.iterator();
+    int index = 0;
+    while(i.hasNext()) {
+      res[index++] = i.next();
+    }
+    return res;
+  }
+
 }
