dd47080d73009221305ce44b6695d72f691d0ffa
wanghao <wanghao1@actionsky.com>
2021-07-16 12:35:42 +0800

    complete question 365. Water and Jug Problem

--- src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
@@ -9,6 +9,7 @@ import java.util.Queue;
 import java.util.TreeSet;
 
 import com.leetcode.entity.NestedInteger;
+import com.leetcode.entity.TreeNode;
 
 public class AlgorithmController361to380 {
 
@@ -229,6 +230,20 @@ public class AlgorithmController361to380 {
       }
     }
     return res;
+  }
+
+  public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+    if (jug1Capacity + jug2Capacity < targetCapacity) {
+      return false;
+    }
+    if (jug1Capacity == 0 || jug2Capacity == 0) {
+      return targetCapacity == 0 || jug2Capacity + jug1Capacity == targetCapacity;
+    }
+    return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+  }
+
+  public int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
   }
 
 }
