c08921981a7d64b905dd7c98f74aff61e3a59603
wanghao <wanghao1@actionsky.com>
2021-07-19 12:12:32 +0800

    complete question 368. Largest Divisible Subset

--- src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
@@ -1,6 +1,7 @@
 package com.leetcode.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -311,6 +312,41 @@ public class AlgorithmController361to380 {
       }
     }
     return false;
+  }
+
+  public List<Integer> largestDivisibleSubset(int[] nums) {
+    Arrays.sort(nums);
+    int len = nums.length;
+    int[] dp = new int[len];
+    dp[0] = 1;
+    Arrays.fill(dp, 1);
+    int maxSize = 1;
+    int maxVal = nums[0];
+    for (int i = 1; i < len; i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] % nums[j] == 0) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      if (dp[i] > maxSize) {
+        maxSize = dp[i];
+        maxVal = nums[i];
+      }
+    }
+
+    List<Integer> res = new ArrayList<>();
+    if (maxSize == 1) {
+      res.add(nums[0]);
+      return res;
+    }
+    for (int i = len - 1; i >= 0; i--) {
+      if (maxVal % nums[i] == 0 && dp[i] == maxSize) {
+        res.add(nums[i]);
+        maxSize--;
+        maxVal = nums[i];
+      }
+    }
+    return res;
   }
 
 }
