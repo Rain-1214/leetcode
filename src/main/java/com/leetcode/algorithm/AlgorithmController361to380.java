7a08e383c90bdbabc195e188d017963125bbb197
wanghao <wanghao1@actionsky.com>
2021-07-29 12:54:18 +0800

    complete question 380. Insert Delete GetRandom O(1)

--- src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Random;
 import java.util.TreeSet;
 
 import com.leetcode.entity.ListNode;
@@ -599,6 +600,56 @@ public class AlgorithmController361to380 {
       }
     }
     return res;
+  }
+
+  class RandomizedSet {
+
+    public Random random;
+    public Map<Integer, Integer> map;
+    public List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+      this.random = new Random();
+      this.map = new HashMap<>();
+      this.list = new ArrayList<>();
+    }
+
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain
+     * the specified element.
+     */
+    public boolean insert(int val) {
+      if (map.containsKey(val)) {
+        return false;
+      }
+      int size = list.size();
+      map.put(val, size);
+      list.add(val);
+      return true;
+    }
+
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified
+     * element.
+     */
+    public boolean remove(int val) {
+      if (!map.containsKey(val)) {
+        return false;
+      }
+      int index = map.get(val);
+      int lastValue = list.get(list.size() - 1);
+      map.put(lastValue, index);
+      list.set(index, lastValue);
+      list.remove(list.size() - 1);
+      map.remove(val);
+      return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+      return list.get(random.nextInt(list.size()));
+    }
   }
 
 }
