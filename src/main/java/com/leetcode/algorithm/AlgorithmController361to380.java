3e87e52a8f9ed0181be93070726366671fc392b3
wanghao <wanghao1@actionsky.com>
2021-07-29 13:24:50 +0800

    complete question 381. Insert Delete GetRandom O(1) - Duplicates allowed

--- src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController361to380.java
@@ -3,12 +3,14 @@ package com.leetcode.algorithm;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.leetcode.entity.ListNode;
@@ -647,6 +649,65 @@ public class AlgorithmController361to380 {
     }
 
     /** Get a random element from the set. */
+    public int getRandom() {
+      return list.get(random.nextInt(list.size()));
+    }
+  }
+
+  class RandomizedCollection {
+
+    public Random random;
+    public Map<Integer, Set<Integer>> map;
+    public List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+      this.random = new Random();
+      this.map = new HashMap<>();
+      this.list = new ArrayList<>();
+    }
+
+    /**
+     * Inserts a value to the collection. Returns true if the collection did not
+     * already contain the specified element.
+     */
+    public boolean insert(int val) {
+      boolean res = !map.containsKey(val);
+      Set<Integer> q = map.getOrDefault(val, new HashSet<Integer>());
+      q.add(list.size());
+      map.put(val, q);
+      list.add(val);
+      return res;
+    }
+
+    /**
+     * Removes a value from the collection. Returns true if the collection contained
+     * the specified element.
+     */
+    public boolean remove(int val) {
+      boolean res = map.containsKey(val);
+      if (!res) {
+        return res;
+      }
+      int len = list.size();
+      int lastValue = list.get(len - 1);
+
+      Set<Integer> lastValueIndex = map.get(lastValue);
+      Set<Integer> currentValueIndex = map.get(val);
+      int currentIndex = currentValueIndex.iterator().next();
+      currentValueIndex.remove(currentIndex);
+
+      lastValueIndex.add(currentIndex);
+      lastValueIndex.remove(len - 1);
+      list.set(currentIndex, lastValue);
+      if (currentValueIndex.isEmpty()) {
+        map.remove(val);
+      }
+      list.remove(len - 1);
+      return res;
+    }
+
+    /** Get a random element from the collection. */
     public int getRandom() {
       return list.get(random.nextInt(list.size()));
     }
