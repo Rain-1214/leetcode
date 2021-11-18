4077f95a8c45eae13f5591ff130d77039a7d5e5a
wanghao <wanghao1@actionsky.com>
2021-11-18 12:26:37 +0800

    complete question 474. Ones and Zeroes

--- src/main/java/com/leetcode/algorithm/AlgorithmController461to480.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController461to480.java
@@ -507,4 +507,26 @@ public class AlgorithmController461to480 {
     return false;
   }
 
+  public int findMaxForm(String[] strs, int m, int n) {
+    int len = strs.length;
+    int[][] dp = new int[m + 1][n + 1];
+    for (int i = 0; i < len; i++) {
+      String str = strs[i];
+      int zeros = 0, ones = 0;
+      for (char c : str.toCharArray()) {
+        if (c == '0') {
+          zeros++;
+        } else {
+          ones++;
+        }
+      }
+      for (int j = m; j >= zeros; j--) {
+        for (int k = n; k >= ones; k--) {
+          dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
+        }
+      }
+    }
+    return dp[m][n];
+  }
+
 }
