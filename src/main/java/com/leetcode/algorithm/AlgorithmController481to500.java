54f2effef7923793242b922528079eea435ac064
wanghao <wanghao1@actionsky.com>
2021-12-03 14:36:52 +0800

    complete question 486. Predict the Winner

--- src/main/java/com/leetcode/algorithm/AlgorithmController481to500.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController481to500.java
@@ -118,4 +118,34 @@ public class AlgorithmController481to500 {
     return Math.max(res, count);
   }
 
+  public boolean PredictTheWinner(int[] nums) {
+    return PredictTheWinner(nums, 0, nums.length - 1, 1) >= 0;
+  }
+
+  public int PredictTheWinner(int[] nums, int left, int right, int turn) {
+    if (left == right) {
+      return nums[left] * turn;
+    }
+    int leftVal = nums[left] * turn + PredictTheWinner(nums, left + 1, right, -turn);
+    int rightVal = nums[right] * turn + PredictTheWinner(nums, left, right - 1, -turn);
+    if (turn == 1) {
+      return Math.max(leftVal, rightVal);
+    } else {
+      return Math.min(leftVal, rightVal);
+    }
+  }
+
+  public boolean PredictTheWinnerII(int[] nums) {
+    int[][] dp = new int[nums.length][nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      dp[i][i] = nums[i];
+    }
+    for (int i = nums.length - 2; i >= 0; i--) {
+      for (int j = i + 1; j < nums.length; j++) {
+        dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+      }
+    }
+    return dp[0][nums.length - 1] >= 0;
+  }
+
 }
