8d606e766d2b5d7d2594bbd6202f0f4e97a6aeb4
wanghao <wanghao1@actionsky.com>
2021-12-13 13:02:12 +0800

    complete question 489. Robot Room Cleaner

---
+++ src/main/java/com/leetcode/entity/Robot.java
@@ -0,0 +1,19 @@
+package com.leetcode.entity;
+
+public class Robot {
+  public boolean move() {
+    return false;
+  }
+
+  // Robot will stay in the same cell after calling turnLeft/turnRight
+  // Each turn will be 90 degrees
+  public void turnLeft() {
+  }
+
+  public void turnRight() {
+  }
+
+  // Clean the current cell
+  public void clean() {
+  }
+}
