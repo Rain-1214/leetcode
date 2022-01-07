aad458d061b955dd375494279889dc8e837b90b2
wanghao <wanghao1@actionsky.com>
2022-01-07 12:41:10 +0800

    complete question 507. Perfect Number

--- src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
@@ -300,4 +300,20 @@ public class AlgorithmController501to520 {
     return res;
   }
 
+  public boolean checkPerfectNumber(int num) {
+    if (num == 1) {
+      return false;
+    }
+    int sum = 1;
+    for (int i = 2; i * i <= num; i++) {
+      if (num % i == 0) {
+        sum += i;
+        if (i * i != num) {
+          sum += num / i;
+        }
+      }
+    }
+    return sum == num;
+  }
+
 }
