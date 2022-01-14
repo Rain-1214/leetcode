428f18eceffd6f72da2b4e5b8e1c9f5c49e28f9f
wanghao <wanghao1@actionsky.com>
2022-01-14 12:52:28 +0800

    complete question 515. Find Largest Value in Each Tree Row

--- src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
@@ -463,4 +463,29 @@ public class AlgorithmController501to520 {
     return res;
   }
 
+  public List<Integer> largestValues(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      int size = q.size();
+      int max = Integer.MIN_VALUE;
+      for (int i = 0; i < size; i++) {
+        TreeNode cur = q.poll();
+        if (cur.left != null) {
+          q.add(cur.left);
+        }
+        if (cur.right != null) {
+          q.add(cur.right);
+        }
+        max = Math.max(max, cur.val);
+      }
+      res.add(max);
+    }
+    return res;
+  }
+
 }
