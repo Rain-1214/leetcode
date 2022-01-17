957d2de65cc3d153efcc294d8df0eb067ea8aa07
wanghao <wanghao1@actionsky.com>
2022-01-17 12:48:40 +0800

    complete question 516. Longest Palindromic Subsequence

--- src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController501to520.java
@@ -488,4 +488,22 @@ public class AlgorithmController501to520 {
     return res;
   }
 
+  public int longestPalindromeSubseq(String s) {
+    char[] sc = s.toCharArray();
+    int[][] dp = new int[sc.length][sc.length];
+    for (int i = 0; i < sc.length; i++) {
+      dp[i][i] = 1;
+    }
+    for (int i = sc.length - 1; i >= 0; i--) {
+      for (int j = i + 1; j < sc.length; j++) {
+        if (sc[i] == sc[j]) {
+          dp[i][j] = dp[i + 1][j - 1] + 2;
+        } else {
+          dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+    return dp[0][sc.length - 1];
+  }
+
 }
