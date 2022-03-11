4fc98250a0d12a78992f56b631ff13ffd85cff76
wanghao <wanghao1@actionsky.com>
2022-03-11 12:51:51 +0800

    complete question 542. 01 Matrix

--- src/main/java/com/leetcode/algorithm/AlgorithmController541to560.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController541to560.java
@@ -22,4 +22,51 @@ public class AlgorithmController541to560 {
     return sc;
   }
 
+  public int[][] updateMatrix(int[][] mat) {
+    int rowMax = mat.length, colMax = mat[0].length;
+    int[][] dp = new int[rowMax][colMax];
+    int flag = (int) Math.pow(10, 7);
+    for (int i = 0; i < rowMax; i++) {
+      for (int j = 0; j < colMax; j++) {
+        if (i == 0 && j == 0) {
+          dp[i][j] = mat[i][j] == 0 ? 0 : flag;
+          continue;
+        }
+        if (mat[i][j] == 0) {
+          dp[i][j] = 0;
+          continue;
+        }
+        if (i == 0) {
+          dp[i][j] = dp[i][j - 1] + 1;
+          continue;
+        }
+        if (j == 0) {
+          dp[i][j] = dp[i - 1][j] + 1;
+          continue;
+        }
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+      }
+    }
+    for (int i = rowMax - 1; i >= 0; i--) {
+      for (int j = colMax - 1; j >= 0; j--) {
+        if (i == rowMax - 1 && j == colMax - 1) {
+          continue;
+        }
+        if (mat[i][j] == 0) {
+          continue;
+        }
+        if (i == rowMax - 1) {
+          dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+          continue;
+        }
+        if (j == colMax - 1) {
+          dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+          continue;
+        }
+        dp[i][j] = Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1]) + 1);
+      }
+    }
+    return dp;
+  }
+
 }
