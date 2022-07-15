b6e4dc8be00392a99f0317f691b35cd07e9d8f2d
wanghao <wanghao1@actionsky.com>
2022-07-15 14:40:45 +0800

    complete question 553. Optimal Division

--- src/main/java/com/leetcode/algorithm/AlgorithmController541to560.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController541to560.java
@@ -520,4 +520,25 @@ public class AlgorithmController541to560 {
     return (int) sum;
   }
 
+  public String optimalDivision(int[] nums) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(nums[0]);
+    if (nums.length > 1) {
+      sb.append('/');
+    }
+    if (nums.length > 2) {
+      sb.append('(');
+    }
+    for (int i = 1; i < nums.length; i++) {
+      sb.append(Integer.toString(nums[i]));
+      if (i < nums.length - 1) {
+        sb.append('/');
+      }
+    }
+    if (nums.length > 2) {
+      sb.append(')');
+    }
+    return sb.toString();
+  }
+
 }
