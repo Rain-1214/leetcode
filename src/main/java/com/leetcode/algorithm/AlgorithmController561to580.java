0927ee2a2ecf0677db1ac45e7599198b32f763d3
wanghao <wanghao1@actionsky.com>
2024-03-18 20:09:04 +0800

    resolve 563

--- src/main/java/com/leetcode/algorithm/AlgorithmController561to580.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController561to580.java
@@ -2,6 +2,8 @@ package com.leetcode.algorithm;
 
 import java.util.Arrays;
 
+import com.leetcode.entity.TreeNode;
+
 public class AlgorithmController561to580 {
 
   public int arrayPairSum(int[] nums) {
@@ -11,6 +13,24 @@ public class AlgorithmController561to580 {
       sum += nums[i];
     }
     return sum;
+  }
+
+  int findTiltResult = 0;
+
+  public int findTilt(TreeNode root) {
+    this.findTiltHelp(root);
+    return this.findTiltResult;
+  }
+
+  public int findTiltHelp(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftSum = this.findTiltHelp(root.left);
+    int rightSum = this.findTiltHelp(root.right);
+    this.findTiltResult += Math.abs(leftSum - rightSum);
+    return leftSum + rightSum + root.val;
+
   }
 
 }
