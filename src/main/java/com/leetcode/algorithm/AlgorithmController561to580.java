4538a7902cb86d882c896816aef7cfa81929a907
wanghao <wanghao1@actionsky.com>
2024-03-20 18:42:39 +0800

    resolve 566

--- src/main/java/com/leetcode/algorithm/AlgorithmController561to580.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController561to580.java
@@ -103,4 +103,25 @@ public class AlgorithmController561to580 {
     return max;
   }
 
+  public int[][] matrixReshape(int[][] mat, int r, int c) {
+    int row = mat.length;
+    int col = mat[0].length;
+    if (row * col != r * c) {
+      return mat;
+    }
+    int[][] res = new int[r][c];
+    int rIndex = 0, cIndex = 0;
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        res[rIndex][cIndex] = mat[i][j];
+        cIndex++;
+        if (cIndex == c) {
+          cIndex = 0;
+          rIndex++;
+        }
+      }
+    }
+    return res;
+  }
+
 }
