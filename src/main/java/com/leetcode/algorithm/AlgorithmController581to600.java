bb2b8e88893a7864ba0b626b19b5ea934caee694
wanghao <wanghao1@actionsky.com>
2025-07-28 13:28:44 +0800

    [new] complete 582

--- src/main/java/com/leetcode/algorithm/AlgorithmController581to600.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController581to600.java
@@ -1,5 +1,12 @@
 package com.leetcode.algorithm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class AlgorithmController581to600 {
 
   public int findUnsortedSubarray(int[] nums) {
@@ -22,5 +29,29 @@ public class AlgorithmController581to600 {
       }
     }
     return right == -1 ? 0 : right - left + 1;
+  }
+
+  public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    for (int i = 0; i < pid.size(); i++) {
+      map.put(pid.get(i), new ArrayList<>());
+    }
+    for (int i = 0; i < ppid.size(); i++) {
+      int parentId = ppid.get(i);
+      if (parentId == 0) {
+        continue;
+      }
+      map.get(parentId).add(pid.get(i));
+    }
+    List<Integer> result = new ArrayList<>();
+    Queue<Integer> queue = new LinkedList<>();
+    queue.add(kill);
+    while (!queue.isEmpty()) {
+      int current = queue.poll();
+      result.add(current);
+      List<Integer> children = map.get(current);
+      queue.addAll(children);
+    }
+    return result;
   }
 }
---
+++ src/test/java/com/leetcode/algorithm/AlgorithmController581to600Test.java
@@ -0,0 +1,26 @@
+package com.leetcode.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class AlgorithmController581to600Test {
+
+  private AlgorithmController581to600 algorithController581to600 = new AlgorithmController581to600();
+
+  @Test
+  public void testKillProcess() {
+    List<Integer> pid = new ArrayList<>();
+    pid.add(1);
+    pid.add(3);
+    pid.add(10);
+    pid.add(5);
+    List<Integer> ppid = new ArrayList<>();
+    ppid.add(3);
+    ppid.add(0);
+    ppid.add(5);
+    ppid.add(3);
+    System.out.println(this.algorithController581to600.killProcess(pid, ppid, 5));
+  }
+}
