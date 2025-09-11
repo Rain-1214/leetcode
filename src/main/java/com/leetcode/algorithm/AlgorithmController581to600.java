8a072bec45d178e9f16a36ec6ca72d58b1a2eaaf
wanghao <wanghao1@actionsky.com>
2025-09-11 13:07:13 +0800

    [question] complete 598

--- src/main/java/com/leetcode/algorithm/AlgorithmController581to600.java
+++ src/main/java/com/leetcode/algorithm/AlgorithmController581to600.java
@@ -534,4 +534,14 @@ public class AlgorithmController581to600 {
     return res;
   }
 
+  public int maxCount(int m, int n, int[][] ops) {
+    int minM = m;
+    int minN = n;
+    for (int[] op : ops) {
+      minM = Math.min(minM, op[0]);
+      minN = Math.min(minN, op[1]);
+    }
+    return minM * minN;
+  }
+
 }
